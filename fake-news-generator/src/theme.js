8a3b9465eb6f9f60e109fbbea1f7099b4c3f4f8f
BTP <titouan.lysandre@gmail.com>
2024-10-16 01:09:47 +0200

    ajout header + theme

--- fake-news-generator/src/theme.js
+++ fake-news-generator/src/theme.js
@@ -1,12 +1,30 @@
+// src/theme.js
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#1976d2",
+      main: "#1F2937", // Bleu foncé
     },
     secondary: {
-      main: "#dc004e",
+      main: "#E63946", // Rouge vif
+    },
+    background: {
+      default: "#E5E7EB", // Gris clair
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+    h1: {
+      fontFamily: "Merriweather, serif",
+      fontSize: "48px",
+      fontWeight: "bold",
+      color: "#1F2937",
+    },
+    body1: {
+      fontFamily: "Roboto, sans-serif",
+      fontSize: "16px",
+      color: "#000000",
     },
   },
 });
