052f1202b4f3b2ab5a49c9e443e589b8fa852061
BTP <titouan.lysandre@gmail.com>
2024-10-17 01:22:09 +0200

    Page home restylisée

--- fake-news-generator/src/pages/Home.js
+++ fake-news-generator/src/pages/Home.js
@@ -2,19 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { useTranslation } from "react-i18next";
+import "../styles/Home.css";
 
 const Home = () => {
   const { t } = useTranslation();
 
   return (
-    <div style={{ padding: "20px" }}>
+    <div className="container">
+      {" "}
       <h1>{t("welcome")}</h1>
-
       <Button
         variant="contained"
         color="primary"
         component={Link}
         to="/generate"
+        className="button"
       >
         {t("startGenerating")}
       </Button>
